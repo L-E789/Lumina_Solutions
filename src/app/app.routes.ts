2f558668fe1194368c6bec4bd78d47e19119446f
jhpenag95 <helver248@hotmail.es>
2025-10-05 12:16:08 -0500

    se realiza creación vista detalles de servicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './pages/home/home';
 import { Services } from './pages/services/services';
 import { Contact } from './pages/contact/contact';
 import { Login } from './pages/login/login';
+import { ServiceDetails } from './pages/service-details/service-details';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: Home },
     { path: 'services', component: Services },
+    { path: 'service-details', component: ServiceDetails },
     { path: 'contact', component: Contact },
     { path: 'login', component: Login },
     { path: '**', redirectTo: '/home' }
